fc4585350b806e01de505728f90eac9318f18369
Ahmet Yeşil <narzac@gmail.com>
2013-06-11 21:22:43 +0300

    add find-file toggle-frame-fullscreen kill-buffer key bindings

--- emacs.d/narzac/bindings.el
+++ emacs.d/narzac/bindings.el
@@ -1,2 +1,4 @@
-;; It's all about the project.
-(global-set-key (kbd "C-x f") nil)
+;; It's all about the prject.
+(global-set-key (kbd "C-x f") 'find-file)
+(global-set-key (kbd "M-f") 'toggle-frame-fullscreen)
+(global-set-key (kbd "M-k") 'kill-buffer)
