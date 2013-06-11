7527b4dad8f6512312d0038aa6323410fe2f1794
Ahmet Yeşil <narzac@gmail.com>
2013-06-12 01:01:26 +0300

    use 'C-x f' for fullscreen

--- emacs.d/narzac/bindings.el
+++ emacs.d/narzac/bindings.el
@@ -1,4 +1,3 @@
 ;; It's all about the prject.
-(global-set-key (kbd "C-x f") 'find-file)
-(global-set-key (kbd "M-f") 'toggle-frame-fullscreen)
+(global-set-key (kbd "C-x f") 'toggle-frame-fullscreen)
 (global-set-key (kbd "M-k") 'kill-buffer)
