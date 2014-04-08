286b06cda244d5f2d7abacdded46705019c3f870
Ahmet Yeşil <narzac@gmail.com>
2014-04-08 11:07:11 +0300

    add web-mode, configure later

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -251,3 +251,27 @@
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 ;; disable auto compile on save
 (setq scss-compile-at-save nil)
+
+;; web-mode
+;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/web-mode/"))
+;; (require 'web-mode)
+;; (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.tpl.html\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+
+;; ;; edit plain HTML files with web-mode
+;; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; ;; indentation
+;; (setq web-mode-markup-indent-offset 2)
+;; (setq web-mode-css-indent-offset 2)
+;; (setq web-mode-code-indent-offset 2)
+;; (setq web-mode-style-padding 1)
+;; (setq web-mode-script-padding 1)
+;; (setq web-mode-block-padding 0)
